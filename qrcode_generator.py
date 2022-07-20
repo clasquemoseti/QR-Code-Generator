9d8d585502e3931bf448e11d0e43546911a09a0d
clasquemoseti <clasquemoseti@gmail.com>
2022-07-20 21:19:23 +0300

    Update qrcode_generator.py

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -3,6 +3,6 @@ import pyqrcode
 
 link = "https://docs.python.org/3.10/library/re.html?highlight=re#module-re"
 qr_code = pyqrcode.create(link)
-qr_code.png("Udemy-course.png", scale=5)
+qr_code.png("re_py_docs.png", scale=5)
 
 qr_code.show()
